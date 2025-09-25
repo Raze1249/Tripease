a8328d1282f242cff5e19b5e81215cb3e2c15d04
Raze1249 <krishbangar392@gmail.com>
2025-09-25 22:11:56 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  fromCity: { type: String, required: true },
+  toCity: { type: String, required: true },
+  travelDate: { type: Date, required: true },
+  seats: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Booking", bookingSchema);
 
