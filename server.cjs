6e2a3e86fdcee983b53c4caaf603efd18a627498
Raze1249 <krishbangar392@gmail.com>
2025-10-18 16:38:17 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 3000;
 
 // --- CONFIGURATION ---
-const MONGODB_URI = 'mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTER_URL>/tripease'; // REPLACE with your actual MongoDB connection string
+const MONGODB_URI = 'mongodb+srv://tripease_user:eb6zKS7H0bpBBC6q@cluster0.faxvovy.mongodb.net/TripeaseDB?retryWrites=true&w=majority&appName=Cluster0'; 
 const DB_NAME = 'tripeaseDB';
 
 // --- AVIATIONSTACK CONFIG ---
